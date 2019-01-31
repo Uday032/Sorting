6cd44f5cad083ca0ff5fd9d89c9667d17e62be22
ua7357 <ua7357@CSELAB02-23.bennett.edu.in>
2019-01-31 09:36:18 +0530

    New Sort

--- NewSort.java
+++ NewSort.java
@@ -1,8 +1,32 @@
 
 public class NewSort {
+	void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int arr= {3,5,2,34,12,6};
+		NewSort obj = new NewSort();
+		obj.sort(arr);
+		System.out.print(arr);
 
 	}
 
